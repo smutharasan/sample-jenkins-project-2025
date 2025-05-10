8b01beb09f57f55ae113a47a67fa6bea8855c5ac
Supriya Mutharasan <59520945+smutharasan@users.noreply.github.com>
2025-05-10 14:14:44 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,16 +1,22 @@
+variable "availability_zone" {
+  default     = "us-east-1a"
+  description = "Availability zone to use when filtering subnets"
+}
+
+# Keep the rest as-is
 variable "aws_region" {
-  default = "us-east-1"
+  default     = "us-east-1"
 }
 
 variable "ami_id" {
-  default = "ami-0230bd60aa48260c6" # Amazon Linux 2 (update if needed)
+  default     = "ami-0230bd60aa48260c6"
 }
 
 variable "instance_type" {
-  default = "t2.micro"
+  default     = "t2.micro"
 }
 
 variable "key_name" {
-  description = "Name of the AWS key pair to use"
-  default     = "your-existing-keypair-name"
+  description = "Name of your existing EC2 key pair"
+  default     = "ECTwoInstanceKeyPairToRunFromMacbookAir2017"
 }
